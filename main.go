4b8223dfd8502b5e41063a4ef700f53dc3987e71
Antonio Azambuja <ilegra.antonio@agibank.com.br>
2020-04-14 20:51:05 -0300

    ref: testing functions

--- main.go
+++ main.go
@@ -5,16 +5,6 @@ import (
 )
 
 func main() {
-	// episode, err := json.Marshal(GetEpisode(5))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// characterIndent, err := json.MarshalIndent(GetCharacter(1), "", "  ")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(string(characterIndent))
-	// characters := GetAllCharacters()
-	location := GetAllLocations()
-	fmt.Println(len(location))
+	multipleEpisodes := GetMultipleEpisodes([]int{1, 2, 3})
+	fmt.Println(multipleEpisodes)
 }
